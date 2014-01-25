1100efa9953991ea6be5b849a74b91a5bb6d1ea0
Greg Roberts <gregorysroberts@gmail.com>
2014-01-25 12:01:38 -0700

    Checks username and password with API

--- script.js
+++ script.js
@@ -33,15 +33,40 @@ $('document').ready(function() {
 
 	$('.signup-form').hide();
 
-	//Bring up signup element when signup is clicked, hide login
 	$('.signup-form-btn').on('click', function(){
 		$('.login-form').hide();
 		$('.signup-form').show();
+		$('.login-form-btn').removeClass('active');
+		$('.signup-form-btn').addClass('active');
 	});
 
 	$('.login-form-btn').on('click', function(){
 		$('.signup-form').hide();
 		$('.login-form').show();
+		$('.signup-form-btn').removeClass('active');
+		$('.login-form-btn').addClass('active');
 	});
+
+	//listen for click
+	$('.btn-login').on('click', function (){
+		//grab .val from username field, and password field and store in variables
+		var loginUser = {};
+		loginUser.username = $('#login-username-field').val();
+		loginUser.password = $('#login-password-field').val();
+
+		console.log((codepen.api.login(loginUser)));
+		//change to not alert if there is not error
+		alert(codepen.api.login(loginUser).error);
+
+		//clear .val on click
+		$('#login-username-field').val('');
+		$('#login-password-field').val('');
+	});
+
+
+	//Show success or failure depending on what the user submited
+
+
+	//If it is a failure, show why
 
 });
